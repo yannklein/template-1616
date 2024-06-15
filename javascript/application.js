6f29675f18c7314d6ad808081e36de86790df633
Yann Klein <yann.klein@me.com>
2024-06-15 10:54:45 +0900

    lecture done

--- javascript/application.js
+++ javascript/application.js
@@ -1,36 +1,39 @@
 console.log("Good Sat!");
 
+const url = "http://www.omdbapi.com/?s=lord of the rings&apikey=adf1f2d7"
+
 // ///////////
 // Vanilla JS
 // ///////////
 
 
-// // 1. Select elements (results)
+// // 1. Select elements (div results)
 // const results = document.querySelector("#results");
-// // 2. No event
-// // 3. Fetch the OMDB API get movie data
-// const url = "https://www.omdbapi.com/?s=harry%20potter&apikey=48727053";
+
+// // 2. Listen to an event: no events
+
+// // 2.5 Fetch the data from API
 // fetch(url)
 //   .then(response => response.json())
-//   .then(data => {
-//     console.log(data);
-//     const movies = data.Search;
-//     // 4. Change DOM (insertAdjacentHTML)
-//     movies.forEach((movie) => {
-//       results.insertAdjacentHTML(
-//         "beforeend", 
-//         `<div class='col-4'>
+//   .then((data) => {
+//     console.log(data.Search);
+//     // 3. Change the DOM, display the cards
+//     const moviesData = data.Search;
+//     moviesData.forEach((movieData) => {
+//       const cardTemplate = `
+//         <div class='col-6 mb-3'>
 //           <div class="card">
-//             <img src="${movie.Poster}" class="card-img-top" alt="${movie.Title}">
+//             <img src="${movieData.Poster}" class="card-img-top" alt="${movieData.Title}">
 //             <div class="card-body">
-//               <h6 class="card-title">${movie.Title}</h6>
-//               <p class="card-text">${movie.Year}</p>
-//               <a href="https://www.imdb.com/title/${movie.imdbID}" class="btn btn-primary" target="_blank">Go on IMDB</a>
+//               <h2 class="card-title">${movieData.Title}</h2>
+//               <p class="card-text">${movieData.Year}</p>
+//               <a href="https://www.imdb.com/title/${movieData.imdbID}" class="btn btn-primary" target="_blank">Go on IMDB</a>
 //             </div>
 //           </div>
-//         </div>`);
+//         </div>`;
+//         results.insertAdjacentHTML("beforeend", cardTemplate);
 //     });
-//   })
+//   });
 
 // ///////////
 // JS with templates
@@ -39,83 +42,85 @@ console.log("Good Sat!");
 // const results = document.querySelector("#results");
 // const template = document.querySelector("#movieCardTemplate");
 
-// // console.log(template.content); // the card itself (HTML element)
-// // console.log(template.innerHTML); // the card itself (String)
+// // console.log(template.content);
+// // console.log(template.innerHTML);
+// // console.log(template.outerHTML);
 
-// const url = "https://www.omdbapi.com/?s=harry%20potter&apikey=48727053";
 // fetch(url)
 //   .then(response => response.json())
 //   .then((data) => {
-//     const movies = data.Search;
-//     movies.forEach((movie) => {
-//       const cardClone = template.content.cloneNode(true) // cloning the card with its content (thanks to the true argument)
-//       cardClone.querySelector("img").src = movie.Poster;
-//       cardClone.querySelector("img").alt = movie.Title;
-//       cardClone.querySelector(".card-title").innerText = movie.Title;
-//       cardClone.querySelector(".card-text").innerText = movie.Year;
-//       cardClone.querySelector("a").href = `https://www.imdb.com/title/${movie.imdbID}`;
-//       results.appendChild(cardClone); // insert the HTML element card to the result HTML element
+//     // console.log(data.Search);
+//     const moviesData = data.Search;
+//     moviesData.forEach((movieData) => {
+//       const templateClone = template.content.cloneNode(true);
+//       // console.log(templateClone);
+//       // console.log(movieData);
+//       templateClone.querySelector("h2").innerText = movieData.Title;
+//       templateClone.querySelector("p").innerText = movieData.Year;
+//       templateClone.querySelector("img").src = movieData.Poster;
+//       templateClone.querySelector("img").alt = movieData.Title;
+//       templateClone.querySelector("a").href = `https://www.imdb.com/title/${movieData.imdbID}`;
+//       // console.log(templateClone);
+//       results.appendChild(templateClone);
 //     });
-//   })
+//   });
 
 // ///////////
 // MUSTACHEJS (simple version)
 // ///////////
 
-// import Mustache from "mustachejs";
+import Mustache from "mustachejs";
 
-// // const templateString = "<h1>{{name}}</h1>";
-// // const data = {name: "Meg"}
-// // const test  = Mustache.render(templateString, data)
-// // console.log(test);
+// const test = Mustache.render(
+//   "<h1>{{title}}</h1>", 
+//   {"title": "Hi from Mustache"}
+// );
+// console.log(test);
+
+// // ///////////
+// // MUSTACHEJS
+// // ///////////
 
 // const results = document.querySelector("#results");
 // const template = document.querySelector("#movieCardTemplate");
 
-// const url = "https://www.omdbapi.com/?s=harry%20potter&apikey=48727053";
+
 // fetch(url)
 //   .then(response => response.json())
 //   .then((data) => {
-//     const movies = data.Search;
-//     movies.forEach((movie) => {
-//       // movie 👇
+//     const moviesData = data.Search;
+//     moviesData.forEach((movieData) => {
+//       const cardTemplate = template.innerHTML;
 //       // {
-//       //   "Title": "Harry Potter and the Deathly Hallows: Part 2",
-//       //   "Year": "2011",
-//       //   "imdbID": "tt1201607",
-//       //   "Type": "movie",
-//       //   "Poster": "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
-//       // }
-//       const card = Mustache.render(template.innerHTML, movie);
-//       console.log(card);
-//       results.insertAdjacentHTML("beforeend", card);
+//       //   "Title": "The Lord of the Rings: The Two Towers",
+//       //   "Year": "2002",
+//       //   "imdbID": "tt0347436",
+//       //   "Type": "game",
+//       //   "Poster": "https://m.media-amazon.com/images/M/MV5BODI0Mzk3OTM4N15BMl5BanBnXkFtZTgwMTM4MTk4MDE@._V1_SX300.jpg"
+//       //   }
+//       const output = Mustache.render(cardTemplate, movieData);
+//       results.insertAdjacentHTML("beforeend", output);
 //     });
 //   });
 
 // ///////////
-// MUSTACHEJS
+// MUSTACHEJS (with iteration)
 // ///////////
-
-// import Mustache from "mustachejs";
 
 // const results = document.querySelector("#results");
 // const template = document.querySelector("#movieCardTemplate");
 
-// const url = "https://www.omdbapi.com/?s=harry%20potter&apikey=48727053";
 // fetch(url)
 //   .then(response => response.json())
 //   .then((data) => {
-//     // data 👇
-//     // {
-//     //   Search: [
-//     //     {Title: "...", },
-//     //     {Title: "...", },
-//     //     {Title: "...", },
-//     //   ]
-//     // }
-//     const moviesData = { "movies": data.Search }
-//     const cards = Mustache.render(template.innerHTML, moviesData);
-//     results.insertAdjacentHTML("beforeend", cards);
+//     const cardTemplate = template.innerHTML;
+//     const moviesData = data.Search;
+//     const formattedData = { movies: moviesData }
+//     // formattedData ==> {movies: [ {Title: "XXX", Poster: "XXX", ...}, {}, {}, {}]}
+
+//     const output = Mustache.render(cardTemplate, formattedData);
+//     console.log(output);
+//     results.innerHTML = output;
 //   });
 
 // ///////////
@@ -127,7 +132,7 @@ import { createApp } from "vue";
 createApp({
   data() {
     return {
-      message: "Hi batch #1383!"
+      message: "Hi batch #1616!"
     }
   }
 }).mount("#vue-example");
